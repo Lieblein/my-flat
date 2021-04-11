02814c4b9a00a3452fdd432437f012b9edc130cf
Ложкина Любовь <llozhkina@htc-cs.ru>
2021-04-11 15:34:56 +0400

    поменяла цвета

--- src/js/facade-with-integrated-handle.js
+++ src/js/facade-with-integrated-handle.js
@@ -21,14 +21,14 @@ AFRAME.registerComponent('facade-with-integrated-handle', {
         const topPart = document.createElement('a-entity');
         const topPartHeight = 3;
         topPart.setAttribute('geometry', `primitive: box; width: ${width}; depth: 2; height: ${topPartHeight}`);
-        topPart.setAttribute('material', 'color: #d6d6d6');
+        topPart.setAttribute('material', 'color: #f1eede');
         topPart.setAttribute('position', `0 ${height / 2 - topPartHeight / 2} -1`);
         el.appendChild(topPart);
 
         const bottomPart = document.createElement('a-entity');
         const bottomPartHeight = height - topPartHeight;
         bottomPart.setAttribute('geometry', `primitive: box; width: ${width}; depth: 2; height: ${bottomPartHeight}`);
-        bottomPart.setAttribute('material', 'color: white');
+        bottomPart.setAttribute('material', 'color: #f1eede');
         bottomPart.setAttribute('position', `0 ${-height / 2 + bottomPartHeight / 2} 1`);
         el.appendChild(bottomPart);
     },
